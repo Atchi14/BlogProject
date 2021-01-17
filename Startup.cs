7de243ee327c2e6d270512f2f59c546feeec3e3d
Atchi14 <77588345+Atchi14@users.noreply.github.com>
2021-01-17 21:42:53 +0100

    Add files via upload
    Blog project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webproj.Startup))]
+namespace webproj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
